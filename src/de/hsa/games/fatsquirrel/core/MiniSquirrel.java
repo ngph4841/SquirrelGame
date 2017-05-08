e4a71b5644757bbcc17a49e388c92a92d5de5fb2
ngph4841 <nghiaduy.pham@hs-augsburg.de>
2017-05-08 17:15:09 +0200

    MiniSquirrel: int tempo & stunCounter

--- src/de/hsa/games/fatsquirrel/core/MiniSquirrel.java
+++ src/de/hsa/games/fatsquirrel/core/MiniSquirrel.java
@@ -1,12 +1,18 @@
 package de.hsa.games.fatsquirrel.core;
 
+import java.util.Random;
+
 public class MiniSquirrel extends Squirrel { // extends squirrel like
     // mastersquirrel
     private int parentId;
+    private int tempo;
+    private int stunCounter;
 
     MiniSquirrel(int id, int energy, XY position, int parentId) {
         super(id, energy, position);
         this.parentId = parentId;
+        this.tempo = 0;
+        this.stunCounter = 0;
     }
 
     public int getParentId() {
@@ -14,6 +20,28 @@ public class MiniSquirrel extends Squirrel { // extends squirrel like
     }
 
     public void nextStep(EntityContext context) throws Exception {
-        context.tryMove(this, position);
+        if (tempo++ == 0) {
+            if (!stun) {
+                Random rn = new Random();
+                int x = 0;
+                int y = 0;
+                int range = 3;
+                x = rn.nextInt(range) + (-1);
+                y = rn.nextInt(range) + (-1);
+
+                XY direction = new XY(x, y);
+
+                context.tryMove(this, direction);
+            } else {
+                stunCounter++;
+                if (stunCounter == 3) {
+                    stunCounter = 0;
+                    cleanse();
+                }
+            }
+        }
+        if (tempo >= 2) {
+            tempo = 0;
+        }
     }
 }
