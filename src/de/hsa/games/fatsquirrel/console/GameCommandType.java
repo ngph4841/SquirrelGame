74479e17019f77c85e24aeee29b2c0d8e83ba76e
ngph4841 <nghiaduy.pham@hs-augsburg.de>
2017-05-09 18:23:05 +0200

    unlocked string method

--- src/de/hsa/games/fatsquirrel/console/GameCommandType.java
+++ src/de/hsa/games/fatsquirrel/console/GameCommandType.java
@@ -2,64 +2,53 @@ package de.hsa.games.fatsquirrel.console;
 
 import de.hsa.games.fatsquirrel.core.XY;
 
-public enum GameCommandType implements CommandTypeInfo{
-
-	
-	HELP("help", "  * list all commands" , "help"),
-	EXIT("exit", "  * exit program", "exit"),
-	ALL("all", " * all", "all"),
-	MASTER_ENERGY("master", " * master energy", "masterMethod"),
-	SPAWN_MINI("mini", " * spawn MiniSquirrel", int.class, "spawn"),
-	UP("5", "  * move up" , XY.class),
-	DOWN("2", "  * move down" , XY.class),
-	LEFT("1", "  * move left", XY.class),
-	RIGHT("3", "  * move right", XY.class);
-	
-
-	private String name;
-	private String info;
-	private String method;
-	private Class<?>[] paramsClasses = new Class[0];
+public enum GameCommandType implements CommandTypeInfo {
 
 
-	private GameCommandType(String name, String info, Class<?> a, String method){
-		this.name = name;
-		this.info = info;
-		this.method = method;
-		paramsClasses = new Class[1];
-		paramsClasses[0] = a;
-	}
-
-	GameCommandType (String name, String info, String method){
-		this.name = name;
-		this.info = info;
-		this.method = method;
-	}
-
-	GameCommandType(String name, String info, Class <?> a){
-		this.name = name;
-		this.info = info;
-		paramsClasses = new Class[1];
-		paramsClasses[0] = a;
-//		paramsClasses[0] = (XY) direction;
-
-	}
-	
-	public String getName(){
-		return name;
-	}
-
-	public String getHelpText(){
-		return info;
-	}
-	
-	public Class<?>[] getParamTypes(){
-		return paramsClasses;
-	}
+    HELP("help", "  * list all commands", "help"),
+    EXIT("exit", "  * exit program", "exit"),
+    ALL("all", " * all", "all"),
+    MASTER_ENERGY("master", " * master energy", "masterMethod"),
+    SPAWN_MINI("mini", " * spawn MiniSquirrel", int.class, "spawn"),
+    UP("5", "  * move up", XY.class, "move"),
+    DOWN("2", "  * move down", XY.class, "move"),
+    LEFT("1", "  * move left", XY.class, "move"),
+    RIGHT("3", "  * move right", XY.class, "move");
 
 
+    private String name;
+    private String info;
+    private String method;
+    private Class<?>[] paramsClasses = new Class[0];
 
-//	public String getMethodName(){
-//		return method;
-//	}
+
+    private GameCommandType(String name, String info, Class<?> a, String method) {
+        this.name = name;
+        this.info = info;
+        this.method = method;
+        paramsClasses = new Class[1];
+        paramsClasses[0] = a;
+    }
+
+    GameCommandType(String name, String info, String method) {
+        this.name = name;
+        this.info = info;
+        this.method = method;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getHelpText() {
+        return info;
+    }
+
+    public Class<?>[] getParamTypes() {
+        return paramsClasses;
+    }
+
+    public String getMethodName() {
+        return method;
+    }
 }
