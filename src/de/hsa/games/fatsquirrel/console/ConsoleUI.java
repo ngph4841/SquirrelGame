e5f8b0a350e1902affaa07058159d941da5f1c90
ngph4841 <nghiaduy.pham@hs-augsburg.de>
2017-05-09 18:34:58 +0200

    started reflection.

--- src/de/hsa/games/fatsquirrel/console/ConsoleUI.java
+++ src/de/hsa/games/fatsquirrel/console/ConsoleUI.java
@@ -17,84 +17,65 @@ import de.hsa.games.fatsquirrel.core.Wall;
 
 public class ConsoleUI implements UI {
 
-	public Command getCommand() throws Exception {
+    public Command getCommand() throws Exception {
 
-		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-		CommandScanner scanner = new CommandScanner(GameCommandType.values(), input);
-		Command command = scanner.next();
-//		GameImpl foo = new GameImpl();
-//		String methodName = command.getCommandType().getMethodName();
-//		Object[] params = command.getParams();
-//		java.lang.reflect.Method method = foo.getClass().getMethod(methodName);
-//		method.invoke(foo, params);
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        CommandScanner scanner = new CommandScanner(GameCommandType.values(), input);
+        Command command = scanner.next();
 
-		return command;
-	}
+        //reflection
+        //TODO: state for gameIMPL ?
+        GameImpl foo = new GameImpl();
+        String methodName = command.getCommandType().getMethodName();
+        Object[] params = command.getParams();
+        java.lang.reflect.Method method = foo.getClass().getMethod(methodName);
+        method.invoke(foo, params);
 
-	public void render(BoardView view) {
-		int boardLength = view.getSize().getY();
-		int boardWidth = view.getSize().getX();
-		int masterEnergy = 0;
-		for (int j = 0; j < boardLength; j++) {
-			for (int i = 0; i < boardWidth; i++) {
-				if (view.getEntityType(j, i) == null) {
-					System.out.print(".");
-					continue;
-				}
-				if (view.getEntityType(j, i) instanceof MasterSquirrel) {
-					System.out.print("M");
-					masterEnergy = view.getEntityType(j, i).getEnergy();
-					continue;
-				}
-				if (view.getEntityType(j, i) instanceof MiniSquirrel) {
-					System.out.print("m");
-					continue;
-				}
-				if (view.getEntityType(j, i) instanceof Wall) {
-					System.out.print("W");
-					continue;
-				}
-				if (view.getEntityType(j, i) instanceof BadBeast) {
-					System.out.print("X");
-					continue;
-				}
-				if (view.getEntityType(j, i) instanceof BadPlant) {
-					System.out.print("P");
-					continue;
-				}
-				if (view.getEntityType(j, i) instanceof GoodBeast) {
-					System.out.print("O");
-					continue;
-				}
-				if (view.getEntityType(j, i) instanceof GoodPlant) {
-					System.out.print("p");
-					continue;
-				}
-			}
-			System.out.println();
-		}
-		System.out.println("HP: " + masterEnergy);
-	}
+        return command;
+    }
+
+    public void render(BoardView view) {
+        int boardLength = view.getSize().getY();
+        int boardWidth = view.getSize().getX();
+        int masterEnergy = 0;
+        for (int j = 0; j < boardLength; j++) {
+            for (int i = 0; i < boardWidth; i++) {
+                if (view.getEntityType(j, i) == null) {
+                    System.out.print(".");
+                    continue;
+                }
+                if (view.getEntityType(j, i) instanceof MasterSquirrel) {
+                    System.out.print("M");
+                    masterEnergy = view.getEntityType(j, i).getEnergy();
+                    continue;
+                }
+                if (view.getEntityType(j, i) instanceof MiniSquirrel) {
+                    System.out.print("m");
+                    continue;
+                }
+                if (view.getEntityType(j, i) instanceof Wall) {
+                    System.out.print("W");
+                    continue;
+                }
+                if (view.getEntityType(j, i) instanceof BadBeast) {
+                    System.out.print("X");
+                    continue;
+                }
+                if (view.getEntityType(j, i) instanceof BadPlant) {
+                    System.out.print("P");
+                    continue;
+                }
+                if (view.getEntityType(j, i) instanceof GoodBeast) {
+                    System.out.print("O");
+                    continue;
+                }
+                if (view.getEntityType(j, i) instanceof GoodPlant) {
+                    System.out.print("p");
+                    continue;
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("HP: " + masterEnergy);
+    }
 }
-
-// switch (direction) {
-// case 1:
-// x = -1;
-// y = 0;
-// break;
-//
-// case 2:
-// x = 0;
-// y = 1;
-// break;
-//
-// case 3:
-// x = 1;
-// y = 0;
-// break;
-//
-// case 5:
-// x = 0;
-// y = -1;
-// break;
-// }
