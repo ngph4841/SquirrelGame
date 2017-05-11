1c46ee82e379f1dd20f3b013a4381fca6988490e
ngph4841 <nghiaduy.pham@hs-augsburg.de>
2017-05-11 19:14:56 +0200

    fps run works kinda?

--- src/de/hsa/games/fatsquirrel/Launcher.java
+++ src/de/hsa/games/fatsquirrel/Launcher.java
@@ -10,18 +10,13 @@ import java.util.TimerTask;
 
 public class Launcher {
     public static void main(String[] args) throws Exception {
-//        XY size = new XY(50, 50);
-//        BoardConfig settings = new BoardConfig(size, 5, 5, 10, 10);
-//        Board board1 = new Board(settings);
-//        State state1 = new State(board1);
-//        Game game1 = new GameImpl(state1);
-//        game1.run();
-// oldmethod
+
         XY size = new XY(50, 50);
         BoardConfig settings = new BoardConfig(size, 5, 5, 10, 10);
         Board board1 = new Board(settings);
         State state1 = new State(board1);
         Game game1 = new GameImpl(state1);
+//        game1.run(); // oldmethod
 
         startGame(game1);
         game1.bufferInput();
@@ -30,7 +25,7 @@ public class Launcher {
     public static void startGame(Game game1)throws Exception{
         Timer timer = new Timer();
         Task t = new Task(game1);
-        timer.schedule(t, 0, 5000);
+        timer.schedule(t, 0, 2000);
     }
 
     static class Task extends TimerTask {
