1903fdad950c65d1a7ce7a6a10c221697ca816c0
ngph4841 <nghiaduy.pham@hs-augsburg.de>
2017-05-11 17:46:52 +0200

    startGame added Task

--- src/de/hsa/games/fatsquirrel/Launcher.java
+++ src/de/hsa/games/fatsquirrel/Launcher.java
@@ -5,13 +5,49 @@ import de.hsa.games.fatsquirrel.core.Board;
 import de.hsa.games.fatsquirrel.core.BoardConfig;
 import de.hsa.games.fatsquirrel.core.XY;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Launcher {
     public static void main(String[] args) throws Exception {
+//        XY size = new XY(50, 50);
+//        BoardConfig settings = new BoardConfig(size, 5, 5, 10, 10);
+//        Board board1 = new Board(settings);
+//        State state1 = new State(board1);
+//        Game game1 = new GameImpl(state1);
+//        game1.run();
+// oldmethod
         XY size = new XY(50, 50);
         BoardConfig settings = new BoardConfig(size, 5, 5, 10, 10);
         Board board1 = new Board(settings);
         State state1 = new State(board1);
         Game game1 = new GameImpl(state1);
-        game1.run();
+
+        startGame(game1);
+        game1.bufferInput();
     }
+
+    public static void startGame(Game game1)throws Exception{
+        Timer timer = new Timer();
+        Task t = new Task(game1);
+        timer.schedule(t, 0, 5000);
+    }
+
+    static class Task extends TimerTask {
+        private Game game1;
+        public Task(Game game1){
+            this.game1 = game1;
+        }
+        @Override
+        public void run() {
+            try {
+                game1.runLive();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+
 }
