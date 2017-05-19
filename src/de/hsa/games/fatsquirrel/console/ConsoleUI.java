f8c434548017dc971ec40f4cc9f90da76d22f6d7
ngph4841 <nghiaduy.pham@hs-augsburg.de>
2017-05-19 16:53:10 +0200

    x,y refacroting

--- src/de/hsa/games/fatsquirrel/console/ConsoleUI.java
+++ src/de/hsa/games/fatsquirrel/console/ConsoleUI.java
@@ -3,17 +3,10 @@ package de.hsa.games.fatsquirrel.console;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
+import de.hsa.games.fatsquirrel.core.*;
 import de.hsa.games.fatsquirrel.util.Command;
 import de.hsa.games.fatsquirrel.util.CommandScanner;
 import de.hsa.games.fatsquirrel.UI;
-import de.hsa.games.fatsquirrel.core.BadBeast;
-import de.hsa.games.fatsquirrel.core.BadPlant;
-import de.hsa.games.fatsquirrel.core.BoardView;
-import de.hsa.games.fatsquirrel.core.GoodBeast;
-import de.hsa.games.fatsquirrel.core.GoodPlant;
-import de.hsa.games.fatsquirrel.core.MasterSquirrel;
-import de.hsa.games.fatsquirrel.core.MiniSquirrel;
-import de.hsa.games.fatsquirrel.core.Wall;
 
 public class ConsoleUI implements UI {
     private BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
@@ -28,54 +21,51 @@ public class ConsoleUI implements UI {
 //       CommandScanner scanner = new CommandScanner(GameCommandType.values(), input);
 //       Command command = scanner.next();
 //        return command;
-
         Command temp = buffer;
         buffer = null;
         return temp;
     }
 
     public void render(BoardView view) {
-        int boardLength = view.getSize().getY();
+        int boardHeight = view.getSize().getY();
         int boardWidth = view.getSize().getX();
         int masterEnergy = 0;
-        for (int j = 0; j < boardLength; j++) {
+        for (int j = 0; j < boardHeight; j++) {
             for (int i = 0; i < boardWidth; i++) {
-                if (view.getEntityType(j, i) == null) {
+                if (view.getEntityType(i, j) == null) {
                     System.out.print(".");
                     continue;
                 }
-                if (view.getEntityType(j, i) instanceof MasterSquirrel) {
+                if (view.getEntityType(i, j) instanceof MasterSquirrel) {
                     System.out.print("M");
-                    masterEnergy = view.getEntityType(j, i).getEnergy();
                     continue;
                 }
-                if (view.getEntityType(j, i) instanceof MiniSquirrel) {
+                if (view.getEntityType(i, j) instanceof MiniSquirrel) {
                     System.out.print("m");
                     continue;
                 }
-                if (view.getEntityType(j, i) instanceof Wall) {
+                if (view.getEntityType(i, j) instanceof Wall) {
                     System.out.print("W");
                     continue;
                 }
-                if (view.getEntityType(j, i) instanceof BadBeast) {
+                if (view.getEntityType(i, j) instanceof BadBeast) {
                     System.out.print("X");
                     continue;
                 }
-                if (view.getEntityType(j, i) instanceof BadPlant) {
+                if (view.getEntityType(i, j) instanceof BadPlant) {
                     System.out.print("P");
                     continue;
                 }
-                if (view.getEntityType(j, i) instanceof GoodBeast) {
+                if (view.getEntityType(i, j) instanceof GoodBeast) {
                     System.out.print("O");
                     continue;
                 }
-                if (view.getEntityType(j, i) instanceof GoodPlant) {
+                if (view.getEntityType(i, j) instanceof GoodPlant) {
                     System.out.print("p");
                     continue;
                 }
             }
             System.out.println();
         }
-        System.out.println("HP: " + masterEnergy);
     }
 }
