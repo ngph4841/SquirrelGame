4ac1d6a6e3c27f390046175358f5b629563e6526
ngph4841 <nghiaduy.pham@hs-augsburg.de>
2017-05-21 16:31:47 +0200

    added MainLogger as a class with static methods

---
+++ src/de/hsa/games/fatsquirrel/util/MainLogger.java
@@ -0,0 +1,43 @@
+package de.hsa.games.fatsquirrel.util;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+/**
+ * Created by Freya on 21.05.2017.
+ */
+public class MainLogger {
+        static Logger logger;
+        public FileHandler fileHandler;
+        SimpleFormatter formatter;
+
+        private MainLogger() throws IOException{
+            //instance the logger
+            logger = Logger.getLogger(MainLogger.class.getName());
+            //instance the filehandler
+            fileHandler = new FileHandler("Log.txt",true);
+            //instance formatter, set formatting, and handler
+            formatter = new SimpleFormatter();
+            fileHandler.setFormatter(formatter);
+            logger.addHandler(fileHandler);
+        }
+
+    private static Logger getLogger(){
+        if(logger == null){
+            try {
+                new MainLogger();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return logger;
+    }
+
+    public static void log(Level level, String msg){
+        getLogger().log(level, msg);
+    }
+
+}
