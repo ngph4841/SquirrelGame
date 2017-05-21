eec33e0e3454398750116a0416e8d1b5ad2d117d
ngph4841 <nghiaduy.pham@hs-augsburg.de>
2017-05-21 21:31:16 +0200

    implemented some code

--- src/de/hsa/games/fatsquirrel/core/MasterSquirrelBot.java
+++ src/de/hsa/games/fatsquirrel/core/MasterSquirrelBot.java
@@ -1,7 +1,10 @@
 package de.hsa.games.fatsquirrel.core;
 
+import de.hsa.games.fatsquirrel.botapi.BotController;
+import de.hsa.games.fatsquirrel.botapi.BotControllerFactoryImpl;
 import de.hsa.games.fatsquirrel.botapi.ControllerContext;
 import de.hsa.games.fatsquirrel.botapi.EntityType;
+import de.hsa.games.fatsquirrel.console.NotEnoughEnergyException;
 
 /**
  * Created by Freya on 19.05.2017.
@@ -12,35 +15,82 @@ public class MasterSquirrelBot extends MasterSquirrel {
     }
 
     @Override
-    public void nextStep(EntityContext context){
-
+    public void nextStep(EntityContext context) throws Exception {
+        ControllerContext controllerContext = new ControllerContextImpl(context,this);
+        BotControllerFactoryImpl botControllerFactory = new BotControllerFactoryImpl(controllerContext, context);
+        BotController botController = botControllerFactory.createMasterBotController();
+        botController.nextStep(controllerContext);
     }
 
     class ControllerContextImpl implements ControllerContext {
+        EntityContext context;
+        MasterSquirrelBot master;
+        private ControllerContextImpl(EntityContext context, MasterSquirrelBot master){
+            this.context = context;
+            this.master = master;
+        }
+
+        public Entity getEntity(){
+            return master;
+        }
 
         @Override
         public XY getViewLowerLeft() {
-            return null;
+            XY size = context.getSize();
+            int x = position.getX() - 15;
+            int y = position.getY() + 15;
+            if(x < 0){ //links klein
+                x = 0;
+            }
+            if(y > size.getY()){ //unten groß
+            y = size.getY()- 1;
+            }
+            return new XY(x,y);
         }
 
         @Override
         public XY getViewUpperRight() {
-            return null;
+            XY size = context.getSize();
+            int x = position.getX() + 15;
+            int y = position.getY() - 15;
+            if(x > size.getX()){ //rechts groß
+                x = size.getX() - 1;
+            }
+            if(y < 0){ //oben klein
+                y = 0;
+            }
+            return new XY(x,y);
         }
 
         @Override
         public EntityType getEntityAt(XY xy) {
-            return null;
+            if(context.getEntityType(xy) instanceof BadBeast){
+                return EntityType.BAD_BEAST;
+            }else if(context.getEntityType(xy) instanceof GoodBeast){
+                return EntityType.GOOD_BEAST;
+            }else if(context.getEntityType(xy) instanceof BadPlant){
+                return EntityType.BAD_PLANT;
+            }else if(context.getEntityType(xy) instanceof GoodPlant){
+                return EntityType.GOOD_PLANT;
+            }else if(context.getEntityType(xy) instanceof Wall){
+                return EntityType.WALL;
+            }else if(context.getEntityType(xy) instanceof MasterSquirrel){
+                return EntityType.MASTER_SQUIRREL;
+            }else if(context.getEntityType(xy) instanceof MiniSquirrel){
+                return EntityType.MINI_SQUIRREL;
+            }else return EntityType.NONE;
         }
 
         @Override
-        public void move(XY direction) {
-
+        public void move(XY direction) throws Exception{
+            context.tryMove((MasterSquirrel) master, direction);
         }
 
         @Override
-        public void spawnMiniBot(XY direction, int energy) {
-
+        public void spawnMiniBot(XY direction, int energy) throws NotEnoughEnergyException {
+            MiniSquirrelBot child = (MiniSquirrelBot) spawnChild(energy);
+            child.setPosition(direction);
+            //TODO wohin mit dem KIND ?
         }
 
         @Override
