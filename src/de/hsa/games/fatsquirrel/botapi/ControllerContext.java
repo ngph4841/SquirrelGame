4582be13166f71254595c4369eb75b578dafab50
ngph4841 <nghiaduy.pham@hs-augsburg.de>
2017-05-21 21:30:58 +0200

    imports & getEntity()

--- src/de/hsa/games/fatsquirrel/botapi/ControllerContext.java
+++ src/de/hsa/games/fatsquirrel/botapi/ControllerContext.java
@@ -1,5 +1,7 @@
 package de.hsa.games.fatsquirrel.botapi;
 
+import de.hsa.games.fatsquirrel.console.NotEnoughEnergyException;
+import de.hsa.games.fatsquirrel.core.Entity;
 import de.hsa.games.fatsquirrel.core.XY;
 
 /**
@@ -9,7 +11,8 @@ public interface ControllerContext {
     public XY getViewLowerLeft();
     public XY getViewUpperRight();
     public EntityType getEntityAt(XY xy);
-    public void move(XY direction);
-    public void spawnMiniBot(XY direction, int energy);
+    public void move(XY direction) throws Exception;
+    public void spawnMiniBot(XY direction, int energy) throws NotEnoughEnergyException;
     public int getEnergy();
+    public Entity getEntity();
 }
