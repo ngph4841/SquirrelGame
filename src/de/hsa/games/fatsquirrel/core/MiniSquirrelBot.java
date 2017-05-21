dbb39a973e58a091649ef2e4ba9f8d3e75a88353
ngph4841 <nghiaduy.pham@hs-augsburg.de>
2017-05-21 21:31:29 +0200

    implemented some methods

--- src/de/hsa/games/fatsquirrel/core/MiniSquirrelBot.java
+++ src/de/hsa/games/fatsquirrel/core/MiniSquirrelBot.java
@@ -1,5 +1,6 @@
 package de.hsa.games.fatsquirrel.core;
 
+import de.hsa.games.fatsquirrel.botapi.BotController;
 import de.hsa.games.fatsquirrel.botapi.ControllerContext;
 import de.hsa.games.fatsquirrel.botapi.EntityType;
 
@@ -13,37 +14,168 @@ public class MiniSquirrelBot extends MiniSquirrel {
 
 
     class ControllerContextImpl implements ControllerContext {
+        EntityContext context;
+        MiniSquirrelBot mini;
+
+        private ControllerContextImpl(EntityContext context, MiniSquirrelBot mini) {
+            this.mini = mini;
+            this.context = context;
+        }
 
         @Override
         public XY getViewLowerLeft() {
-            return null;
+            XY size = context.getSize();
+            int x = position.getX() - 10;
+            int y = position.getY() + 10;
+            if (x < 0) { //links klein
+                x = 0;
+            }
+            if (y > size.getY()-1) { //unten groß
+                y = size.getY();
+            }
+            return new XY(x, y);
         }
 
         @Override
         public XY getViewUpperRight() {
-            return null;
+            XY size = context.getSize();
+            int x = position.getX() + 10;
+            int y = position.getY() - 10;
+            if (x > size.getX() - 1) { //rechts groß
+                x = size.getX();
+            }
+            if (y < 0) { //oben klein
+                y = 0;
+            }
+            return new XY(x, y);
         }
 
         @Override
         public EntityType getEntityAt(XY xy) {
-            return null;
+            if (context.getEntityType(xy) instanceof BadBeast) {
+                return EntityType.BAD_BEAST;
+            } else if (context.getEntityType(xy) instanceof GoodBeast) {
+                return EntityType.GOOD_BEAST;
+            } else if (context.getEntityType(xy) instanceof BadPlant) {
+                return EntityType.BAD_PLANT;
+            } else if (context.getEntityType(xy) instanceof GoodPlant) {
+                return EntityType.GOOD_PLANT;
+            } else if (context.getEntityType(xy) instanceof Wall) {
+                return EntityType.WALL;
+            } else if (context.getEntityType(xy) instanceof MasterSquirrel) {
+                return EntityType.MASTER_SQUIRREL;
+            } else if (context.getEntityType(xy) instanceof MiniSquirrel) {
+                return EntityType.MINI_SQUIRREL;
+            } else return EntityType.NONE;
         }
 
         @Override
-        public void move(XY direction) {
-
+        public void move(XY direction) throws Exception {
+            context.tryMove((MiniSquirrel) mini, direction);
         }
 
         @Override
         public void spawnMiniBot(XY direction, int energy) {
-
+            //nope mini cant spawn mini...
         }
 
         @Override
         public int getEnergy() {
-            return 0;
+            return mini.getEnergy();
         }
+
+        @Override
+        public Entity getEntity() {
+            return mini;
+        }
+
+        public void explode(int impactRadius) { //double? & TODO fix this ugly child plz ....
+            double impactArea = impactRadius * impactRadius * 3.14;
+            int distance = 0;
+            int x = mini.getPosition().getX();
+            int y = mini.getPosition().getY();
+
+            Entity[] entities = new Entity[(int) impactArea];
+            int entitesAmount = 0;
+            double energyLoss = 200 * (mini.getEnergy() / impactArea) * (1 - distance / impactRadius);
+            int counter = (impactRadius - 1) * 2;
+            for (int i = 1; i <= impactRadius; i++) {
+                for (int j = counter; j > 0; j--) {
+                    XY temp = new XY(x + j, y + i);
+                    switch (getEntityAt(temp)) {
+                        case NONE:
+                            break;
+                        case WALL:
+                            break;
+                        case GOOD_BEAST:
+                        case BAD_BEAST:
+                        case BAD_PLANT:
+                        case GOOD_PLANT:
+                        case MINI_SQUIRREL:
+                        case MASTER_SQUIRREL:
+                            entities[entitesAmount++] = context.getEntityType(temp);
+                    }
+                }
+                counter -= 2;
+            }
+
+            for (int i = 1; i < impactRadius; i++) {
+                switch (getEntityAt(new XY(x + i, y))) {
+                    case NONE:
+                        break;
+                    case WALL:
+                        break;
+                    case GOOD_BEAST:
+                    case BAD_BEAST:
+                    case BAD_PLANT:
+                    case GOOD_PLANT:
+                    case MINI_SQUIRREL:
+                    case MASTER_SQUIRREL:
+                        entities[entitesAmount++] = context.getEntityType(new XY(x + i, y));
+                }
+            }
+
+            for (int i = -1; i > -1 * impactRadius; i--) {
+                switch (getEntityAt(new XY(x + i, y))) {
+                    case NONE:
+                        break;
+                    case WALL:
+                        break;
+                    case GOOD_BEAST:
+                    case BAD_BEAST:
+                    case BAD_PLANT:
+                    case GOOD_PLANT:
+                    case MINI_SQUIRREL:
+                    case MASTER_SQUIRREL:
+                        entities[entitesAmount++] = context.getEntityType(new XY(x + i, y));
+                }
+            }
+
+            counter = (impactRadius - 1) * 2;
+            for (int i = -1; i >= -1*impactRadius; i--) {
+                for (int j = counter; j > 0; j--) {
+                    XY temp = new XY(x + j, y + i);
+                    switch (getEntityAt(temp)) {
+                        case NONE:
+                            break;
+                        case WALL:
+                            break;
+                        case GOOD_BEAST:
+                        case BAD_BEAST:
+                        case BAD_PLANT:
+                        case GOOD_PLANT:
+                        case MINI_SQUIRREL:
+                        case MASTER_SQUIRREL:
+                            entities[entitesAmount++] = context.getEntityType(temp);
+                    }
+                }
+                counter -= 2;
+            }
+        }
+
+
     }
+
 }
 
 
