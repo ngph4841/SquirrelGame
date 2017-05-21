8e44a9bda34b8fa67c17bd99d17cd1daf3b854db
ngph4841 <nghiaduy.pham@hs-augsburg.de>
2017-05-21 21:55:22 +0200

    formatting

--- src/de/hsa/games/fatsquirrel/core/MasterSquirrelBot.java
+++ src/de/hsa/games/fatsquirrel/core/MasterSquirrelBot.java
@@ -1,9 +1,6 @@
 package de.hsa.games.fatsquirrel.core;
 
-import de.hsa.games.fatsquirrel.botapi.BotController;
-import de.hsa.games.fatsquirrel.botapi.BotControllerFactoryImpl;
-import de.hsa.games.fatsquirrel.botapi.ControllerContext;
-import de.hsa.games.fatsquirrel.botapi.EntityType;
+import de.hsa.games.fatsquirrel.botapi.*;
 import de.hsa.games.fatsquirrel.console.NotEnoughEnergyException;
 
 /**
@@ -16,7 +13,7 @@ public class MasterSquirrelBot extends MasterSquirrel {
 
     @Override
     public void nextStep(EntityContext context) throws Exception {
-        ControllerContext controllerContext = new ControllerContextImpl(context,this);
+        ControllerContext controllerContext = new ControllerContextImpl(context, this);
         BotControllerFactoryImpl botControllerFactory = new BotControllerFactoryImpl(controllerContext, context);
         BotController botController = botControllerFactory.createMasterBotController();
         botController.nextStep(controllerContext);
@@ -25,12 +22,13 @@ public class MasterSquirrelBot extends MasterSquirrel {
     class ControllerContextImpl implements ControllerContext {
         EntityContext context;
         MasterSquirrelBot master;
-        private ControllerContextImpl(EntityContext context, MasterSquirrelBot master){
+
+        private ControllerContextImpl(EntityContext context, MasterSquirrelBot master) {
             this.context = context;
             this.master = master;
         }
 
-        public Entity getEntity(){
+        public Entity getEntity() {
             return master;
         }
 
@@ -39,13 +37,13 @@ public class MasterSquirrelBot extends MasterSquirrel {
             XY size = context.getSize();
             int x = position.getX() - 15;
             int y = position.getY() + 15;
-            if(x < 0){ //links klein
+            if (x < 0) { //links klein
                 x = 0;
             }
-            if(y > size.getY()){ //unten groß
-            y = size.getY()- 1;
+            if (y > size.getY()) { //unten groß
+                y = size.getY() - 1;
             }
-            return new XY(x,y);
+            return new XY(x, y);
         }
 
         @Override
@@ -53,36 +51,46 @@ public class MasterSquirrelBot extends MasterSquirrel {
             XY size = context.getSize();
             int x = position.getX() + 15;
             int y = position.getY() - 15;
-            if(x > size.getX()){ //rechts groß
+            if (x > size.getX()) { //rechts groß
                 x = size.getX() - 1;
             }
-            if(y < 0){ //oben klein
+            if (y < 0) { //oben klein
                 y = 0;
             }
-            return new XY(x,y);
+            return new XY(x, y);
         }
 
         @Override
         public EntityType getEntityAt(XY xy) {
-            if(context.getEntityType(xy) instanceof BadBeast){
+            XY viewUpperRight = getViewUpperRight();
+            XY viewLowerLeft = getViewLowerLeft();
+
+            if (xy.getX() > viewUpperRight.getX() | xy.getX() < viewLowerLeft.getX()) {
+                throw new OutOfViewException();
+            } else if (xy.getY() > viewLowerLeft.getY() | xy.getY() < viewUpperRight.getY()) {
+                throw new OutOfViewException();
+            }
+
+
+            if (context.getEntityType(xy) instanceof BadBeast) {
                 return EntityType.BAD_BEAST;
-            }else if(context.getEntityType(xy) instanceof GoodBeast){
+            } else if (context.getEntityType(xy) instanceof GoodBeast) {
                 return EntityType.GOOD_BEAST;
-            }else if(context.getEntityType(xy) instanceof BadPlant){
+            } else if (context.getEntityType(xy) instanceof BadPlant) {
                 return EntityType.BAD_PLANT;
-            }else if(context.getEntityType(xy) instanceof GoodPlant){
+            } else if (context.getEntityType(xy) instanceof GoodPlant) {
                 return EntityType.GOOD_PLANT;
-            }else if(context.getEntityType(xy) instanceof Wall){
+            } else if (context.getEntityType(xy) instanceof Wall) {
                 return EntityType.WALL;
-            }else if(context.getEntityType(xy) instanceof MasterSquirrel){
+            } else if (context.getEntityType(xy) instanceof MasterSquirrel) {
                 return EntityType.MASTER_SQUIRREL;
-            }else if(context.getEntityType(xy) instanceof MiniSquirrel){
+            } else if (context.getEntityType(xy) instanceof MiniSquirrel) {
                 return EntityType.MINI_SQUIRREL;
-            }else return EntityType.NONE;
+            } else return EntityType.NONE;
         }
 
         @Override
-        public void move(XY direction) throws Exception{
+        public void move(XY direction) throws Exception {
             context.tryMove((MasterSquirrel) master, direction);
         }
 
