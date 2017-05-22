8b8d62a065d313872fd38cea2d08a211325744b3
ngph4841 <nghiaduy.pham@hs-augsburg.de>
2017-05-22 13:26:18 +0200

    deleted methods

--- src/de/hsa/games/fatsquirrel/botapi/BotControllerFactoryImpl.java
+++ src/de/hsa/games/fatsquirrel/botapi/BotControllerFactoryImpl.java
@@ -1,25 +1,19 @@
 package de.hsa.games.fatsquirrel.botapi;
 
-import de.hsa.games.fatsquirrel.core.Entity;
-import de.hsa.games.fatsquirrel.core.EntityContext;
-
 /**
  * Created by Freya on 19.05.2017.
  */
 public class BotControllerFactoryImpl implements BotControllerFactory{ //in die Gameimpl zum erstellen der botcontroller
-    private ControllerContext controllerContext;
-
-    public BotControllerFactoryImpl(ControllerContext controllerContext){
-        this.controllerContext = controllerContext;
+    public BotControllerFactoryImpl(){
     }
 
     @Override
     public BotController createMasterBotController() {
-        return new BotControllerImpl(controllerContext);
+        return new BotControllerImpl();
     }
 
     @Override
     public BotController createMiniBotController() {
-        return new BotControllerImpl(controllerContext);
+        return new BotControllerImpl();
     }
 }
