c854d5527cebb955a474559495692cbef46f8df6
ngph4841 <nghiaduy.pham@hs-augsburg.de>
2017-06-02 15:13:22 +0200

    mode is public&static now so we can switch between bots&SP

--- src/main/java/de/hsa/games/fatsquirrel/Launcher.java
+++ src/main/java/de/hsa/games/fatsquirrel/Launcher.java
@@ -2,10 +2,7 @@ package de.hsa.games.fatsquirrel;
 
 import de.hsa.games.fatsquirrel.console.ConsoleUI;
 import de.hsa.games.fatsquirrel.console.GameImpl;
-import de.hsa.games.fatsquirrel.core.Board;
-import de.hsa.games.fatsquirrel.core.BoardConfig;
-import de.hsa.games.fatsquirrel.core.MasterSquirrelBot;
-import de.hsa.games.fatsquirrel.core.XY;
+import de.hsa.games.fatsquirrel.core.*;
 import de.hsa.games.fatsquirrel.gui.FxUI;
 import javafx.application.Application;
 import javafx.event.EventHandler;
@@ -22,53 +19,66 @@ import java.util.logging.SimpleFormatter;
 
 public class Launcher extends Application {
     public static BoardConfig settings = new BoardConfig(new XY(50, 50), 5, 5, 10, 10);
+    public static int mode = 3;
+
 
 //    public static Logger logger = Logger.getLogger(Launcher.class.getSimpleName());
 //    private FileHandler fileHandler = new FileHandler("Log.txt");
 //    private SimpleFormatter formatter = new SimpleFormatter();
-
     public Launcher() throws IOException {
     }
 
     public static void main(String[] args) throws Exception {
-        Board board1 = new Board(settings);
-        State state1 = new State(board1);
-        int mode = 2;
+        Board board = new Board(settings);
+        State state;
         Game game;
-        switch(mode){
+
+        switch (mode) {
             case 0: //1frame per input
-                game = new GameImpl(state1, new ConsoleUI());
+                board.addPlayer(new MasterSquirrel(1000, new XY(settings.getSize().x / 2, settings.getSize().y / 2)));
+                board.fillSet();
+                state = new State(board);
+                game = new GameImpl(state, new ConsoleUI());
                 game.run();
-                break;
             case 1: //alot of frames
-                game = new GameImpl(state1, new ConsoleUI());
+                board.addPlayer(new MasterSquirrel(1000, new XY(settings.getSize().x / 2, settings.getSize().y / 2)));
+                board.fillSet();
+                state = new State(board);
+                game = new GameImpl(state, new ConsoleUI());
                 startGame(game);
-                while(true) {
+                while (true) {
                     game.bufferInput();
                 }
-            case 2: //javafx
-                game = new GameImpl(state1, FxUI.createInstance(settings.getSize()));
-                Application.launch(args);
-                game.bufferInput();
-                break;
+            case 2: //javafx SinglePlayer and Bots
             case 3:
-                Board board2 = board1;
-                board2.getEntitySet().add(new MasterSquirrelBot(3000,2000,new XY(40,40)));
-                State state2 = new State(board2);
-
-                game = new GameImpl(state2, FxUI.createInstance(settings.getSize()));
                 Application.launch(args);
-                game.bufferInput();
+                break;
         }
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        //...
         FxUI fxUI = FxUI.createInstance(settings.getSize());
         Board board = new Board(settings);
+
+        switch(mode){
+            case 2:
+                //singlePlayer
+                board.addPlayer(new MasterSquirrel(2000, 2000, new XY(settings.getSize().x / 2, settings.getSize().y / 2)));
+                break;
+            case 3:
+                //1bot
+                //board.addPlayer(new MasterSquirrelBot(2000, 2000, new XY(settings.getSize().x / 2, settings.getSize().y / 2)));
+
+                //2bot
+                board.addPlayer(new MasterSquirrelBot(2000, 2000, new XY(settings.getSize().x - 2, settings.getSize().y - 2)));
+                board.addPlayer(new MasterSquirrelBot(3000, 2000, new XY(2, 2)));
+                break;
+        }
+
+        board.fillSet();
         State state = new State(board);
-        final Game game = new GameImpl(state,fxUI);
+        final Game game = new GameImpl(state, fxUI);
 
         primaryStage.setScene(fxUI);
         primaryStage.setTitle("Diligent Squirrel");
