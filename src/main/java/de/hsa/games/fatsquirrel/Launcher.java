8076d0f80d71e37c88f7b8ab28cfbce5c7774350
ngph4841 <nghiaduy.pham@hs-augsburg.de>
2017-06-04 22:14:54 +0200

    logger

--- src/main/java/de/hsa/games/fatsquirrel/Launcher.java
+++ src/main/java/de/hsa/games/fatsquirrel/Launcher.java
@@ -5,32 +5,41 @@ import de.hsa.games.fatsquirrel.console.GameImpl;
 import de.hsa.games.fatsquirrel.core.*;
 import de.hsa.games.fatsquirrel.gui.FxUI;
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 
 import java.io.IOException;
-import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.logging.FileHandler;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
 public class Launcher extends Application {
     public static BoardConfig settings = new BoardConfig(new XY(50, 50), 5, 5, 10, 10, 1000,
-            "de.hsa.games.fatsquirrel.botimpls.BotControllerMaster","de.hsa.fatsquirrel.botimpls.BotControllerMini");
+            "de.hsa.games.fatsquirrel.botimpls.BotControllerMaster", "de.hsa.fatsquirrel.botimpls.BotControllerMini");
     public static int mode = 3;
 
+    private static Logger launcherLogger;
+    static {
+        launcherLogger = Logger.getLogger(Launcher.class.getSimpleName());
+        FileHandler fileHandler = null;
+        try {
+            fileHandler = new FileHandler("Log.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        SimpleFormatter formatter = new SimpleFormatter();
+        fileHandler.setFormatter(formatter);
+        launcherLogger.addHandler(fileHandler);
+    }
 
-//    public static Logger logger = Logger.getLogger(Launcher.class.getSimpleName());
-//    private FileHandler fileHandler = new FileHandler("Log.txt");
-//    private SimpleFormatter formatter = new SimpleFormatter();
     public Launcher() throws IOException {
     }
 
     public static void main(String[] args) throws Exception {
-        Board board = new Board(settings,mode);
+        Board board = new Board(settings, mode);
         State state;
         Game game;
 
@@ -48,6 +57,7 @@ public class Launcher extends Application {
                 }
             case 2: //javafx SinglePlayer and Bots
             case 3:
+                launcherLogger.log(Level.INFO,"Game started in javaFx!");
                 Application.launch(args);
                 break;
         }
@@ -56,7 +66,7 @@ public class Launcher extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FxUI fxUI = FxUI.createInstance(settings.getSize());
-        Board board = new Board(settings,mode);
+        Board board = new Board(settings, mode);
         State state = new State(board);
         final Game game = new GameImpl(state, fxUI);
 
